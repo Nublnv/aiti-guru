91fe25d465c283b6cc2eccfde945499c3e087d73
Mikhail Golyachkov <cp7reg@gmail.com>
2026-02-10 19:13:57 +0300

    Add new route due task

--- src/python/service/routes.py
+++ src/python/service/routes.py
@@ -1,15 +1,36 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Request
+from fastapi.responses import JSONResponse
 from .dependency import Psql
-from sqlalchemy import text
+from .schemas import AddItemToOrderRequest
+from .models import add_item_into_order
+from .errors import AppError
 
 app = FastAPI()
 
-@app.get(
-    "/",
+@app.exception_handler(Exception)
+@app.exception_handler(500)
+async def internal_exception_handler(
+    _: Request,
+    exc: Exception,
+
+) -> JSONResponse:
+    return JSONResponse(
+        status_code=500,
+        content={"message": str(exc)}
+    )
+
+@app.post(
+    "/add_item_to_order",
     response_model=dict
     )
-async def read_root(
-    psql: Psql
+async def add_item_to_order(
+    psql: Psql,
+    data: AddItemToOrderRequest = Body(...)
 ):
-    await psql.execute(text("SELECT 1"))
-    return {"message": "Hello, World!"}
+    await add_item_into_order(
+        session=psql,
+        order_id=data.order_id,
+        item_id=data.item_id,
+        quantity=data.quantity
+    )
+    return {"message": f"Added {data.quantity} of item {data.item_id} into order {data.order_id}"}
